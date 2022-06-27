165227d43f1c8a7125b7fe5cb5bafe618c343b62
Mark Sharp <mark.sharp@solent.ac.uk>
2022-06-27 11:31:12 +0100

    Added missing language string for capabilities

--- lang/en/block_succeed.php
+++ lang/en/block_succeed.php
@@ -2,4 +2,5 @@
 $string['pluginname'] = 'succeed@solent';
 $string['succeed:addinstance'] = 'Add succeed@solent block';
 $string['succeed:editpoll'] = 'Edit succeed@solent poll';
+$string['succeed:myaddinstance'] = 'Add succeed@solent block';
 $string['succeed_title'] = 'succeed@solent';
