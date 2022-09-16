0fdcb247cd65e898d0d123ad19697c7480279ade
Mark Sharp <mark.sharp@solent.ac.uk>
2022-09-16 11:57:18 +0100

    Fix coding standards

--- db/access.php
+++ db/access.php
@@ -1,19 +1,40 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Access file
+ *
+ * @package   block_succeed
+ * @author    Mark Sharp <mark.sharp@solent.ac.uk>
+ * @copyright 2022 Solent University {@link https://www.solent.ac.uk}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
-
     'block/succeed:myaddinstance' => array(
         'captype' => 'write',
         'contextlevel' => CONTEXT_SYSTEM,
         'archetypes' => array(
             'user' => CAP_ALLOW
         ),
-
         'clonepermissionsfrom' => 'moodle/my:manageblocks'
     ),
-	
-	'block/succeed:editpoll' => array(
-
+    'block/succeed:editpoll' => array(
         'captype' => 'write',
         'contextlevel' => CONTEXT_BLOCK,
         'archetypes' => array(
@@ -21,8 +42,7 @@ $capabilities = array(
             'manager' => CAP_ALLOW
         )
     ),
-	 'block/succeed:addinstance' => array(
-
+     'block/succeed:addinstance' => array(
         'captype' => 'write',
         'contextlevel' => CONTEXT_BLOCK,
         'archetypes' => array(
@@ -30,6 +50,4 @@ $capabilities = array(
             'manager' => CAP_ALLOW
         )
     ),
-	
-
 );
