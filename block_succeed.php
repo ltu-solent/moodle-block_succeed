1255ead52aded55d866014b5edf3cc6cfe7eae05
Mark Sharp <mark.sharp@solent.ac.uk>
2022-09-16 14:50:53 +0100

    Add new links, and update links to use vanity urls

--- block_succeed.php
+++ block_succeed.php
@@ -34,97 +34,82 @@ class block_succeed extends block_base {
         }
 
         $this->content = new stdClass();
-        $this->content->text = '<p>
-    <a href="https://learn.solent.ac.uk/course/view.php?id=90">
-    <img src="/blocks/succeed/images/home.png" alt="" width="25" height="25" role="presentation">&nbsp;Succeed Home Page</a>
-    </p><br>
-    <h4>The Assignment Process:</h4>
+        $this->content->text = '
     <p>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31447&amp;section=1">
-            <img src="/blocks/succeed/images/1-assignment-briefs-icon.png" alt="" width="25" height="25" role="presentation">
-        </a>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31627&amp;section=0">1. Starting Assignments</a>
+        <img src="/blocks/succeed/images/home.png" alt="" width="25" height="25" role="presentation">
+        <a href="https://learn.solent.ac.uk/succeed">Succeed Home Page</a>
     </p>
     <p>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31447&amp;section=2">
-            <img src="/blocks/succeed/images/2-finding-resources.png" alt="" width="25" height="25" role="presentation">
-        </a>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31629&amp;section=0">2. Finding Sources&nbsp;</a>
-    </p>
-    <p>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31447&amp;section=3">
-            <img src="/blocks/succeed/images/3-using-resources-icon.png" alt="" width="25" height="25" role="presentation">
-        </a>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31628&amp;section=0">3. Using Sources&nbsp;</a>
-    </p>
-    <p>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31447&amp;section=4">
-            <img src="/blocks/succeed/images/4-planning-and-development-icon.png" alt="" width="25" height="25" role="presentation">
-        </a>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31630&amp;section=0">4.&nbsp;Critical Thinking and Planning&nbsp;</a>
-    </p>
-    <p>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31447&amp;section=5">
-            <img src="/blocks/succeed/images/5-writing-the-assignment-icon.png" alt="" width="25" height="25" role="presentation">
-        </a>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31631&amp;section=0">5. Writing an Assignment</a>
-    </p>
-    <p>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31447&amp;section=7">
-            <img src="/blocks/succeed/images/6-referencing-icon.png" alt="" width="25" height="25" role="presentation">
-        </a>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31632&amp;section=0">6. Referencing</a>
-    </p>
-    <p>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31447&amp;section=9">
-            <img src="/blocks/succeed/images/7-reflection-feedback-icon.png" alt="" width="25" height="25" role="presentation">
-        </a>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31636&amp;section=0">7. Feedback and Reflection</a>
-    </p>
-    <p>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31447&amp;section=10">
-            <img src="/blocks/succeed/images/8-dissertations-icon.png" alt="" width="25" height="25" role="presentation">
-        </a>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31634&amp;section=0">8. Dissertations and Major Projects</a>
+        <img src="/blocks/succeed/images/get-ready.svg" alt="" width="25" height="25" role="presentation" />
+        <a href="https://learn.solent.ac.uk/getreadytolearn">Get Ready to Learn</a>
     </p>
     <br>
-    <h4>Professional Skills:</h4>
+    <h4>The Assignment Process</h4>
     <p>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31447&amp;section=11">
-            <img src="/blocks/succeed/images/teamwork-icon.png" alt="" width="25" height="25" role="presentation">
-        </a>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31690&amp;section=0">Group Work</a>
+        <img src="/blocks/succeed/images/1-assignment-briefs-icon.png" alt="" width="25" height="25" role="presentation">
+        <a href="https://learn.solent.ac.uk/starting-assignments">Starting Assignments</a>
     </p>
     <p>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31447&amp;section=12">
-            <img src="/blocks/succeed/images/organising-time-icon.png" alt="" width="21" height="25" role="presentation">
-        </a>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31635&amp;section=0">Managing Time and Stress</a>
+        <img src="/blocks/succeed/images/2-finding-resources.png" alt="" width="25" height="25" role="presentation">
+        <a href="https://learn.solent.ac.uk/finding-sources">Finding Sources</a>
     </p>
     <p>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31447&amp;section=6">
-            <img src="/blocks/succeed/images/presentations-icon.png" alt="" width="25" height="26" role="presentation">
-        </a>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31637&amp;section=0">Giving Presentations</a>
+        <img src="/blocks/succeed/images/3-using-resources-icon.png" alt="" width="25" height="25" role="presentation">
+        <a href="https://learn.solent.ac.uk/using-sources">Using Sources</a>
     </p>
     <p>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31447&amp;section=13">
-            <img src="/blocks/succeed/images/grammar-lauhuage-icon.png" alt="" width="25" height="23" role="presentation">
-        </a>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31649&amp;section=0">Grammar and Language Skills</a>
+        <img src="/blocks/succeed/images/4-planning-and-development-icon.png" alt="" width="25" height="25" role="presentation">
+        <a href="https://learn.solent.ac.uk/critical-thinking">Critical Thinking and Planning</a>
     </p>
     <p>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31447&amp;section=14">
-            <img src="/blocks/succeed/images/maths-skills-icon.png" alt="" width="19" height="25" role="presentation">
-        </a>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31689&amp;section=0">Maths and Statistics</a>
+        <img src="/blocks/succeed/images/5-writing-the-assignment-icon.png" alt="" width="25" height="25" role="presentation">
+        <a href="https://learn.solent.ac.uk/writing">Writing an Assignment</a>
     </p>
     <p>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31447&amp;section=15">
-            <img src="/blocks/succeed/images/professional-practice-icon.png" alt="" width="21" height="25" role="presentation">
-        </a>
-        <a href="https://learn.solent.ac.uk/course/view.php?id=31691&amp;section=0">Professional Presence</a>
-    </p>';
+        <img src="/blocks/succeed/images/6-referencing-icon.png" alt="" width="25" height="25" role="presentation">
+        <a href="https://learn.solent.ac.uk/referencing">Referencing</a>
+    </p>
+    <p>
+        <img src="/blocks/succeed/images/7-reflection-feedback-icon.png" alt="" width="25" height="25" role="presentation">
+        <a href="https://learn.solent.ac.uk/feedback-reflection">Feedback and Reflection</a>
+    </p>
+    <p>
+        <img src="/blocks/succeed/images/8-dissertations-icon.png" alt="" width="25" height="25" role="presentation">
+        <a href="https://learn.solent.ac.uk/dissertations">Dissertations and Major Projects</a>
+    </p>
+    <br>
+    <h4>Professional Skills</h4>
+    <p>
+        <img src="/blocks/succeed/images/teamwork-icon.png" alt="" width="25" height="25" role="presentation">
+        <a href="https://learn.solent.ac.uk/group-work">Group Work</a>
+    </p>
+    <p>
+        <img src="/blocks/succeed/images/organising-time-icon.png" alt="" width="21" height="25" role="presentation">
+        <a href="https://learn.solent.ac.uk/time-stress">Managing Time and Stress</a>
+    </p>
+    <p>
+        <img src="/blocks/succeed/images/presentations-icon.png" alt="" width="25" height="26" role="presentation">
+        <a href="https://learn.solent.ac.uk/presentations">Giving Presentations</a>
+    </p>
+    <p>
+        <img src="/blocks/succeed/images/grammar-lauhuage-icon.png" alt="" width="25" height="23" role="presentation">
+        <a href="https://learn.solent.ac.uk/grammar">Grammar and Language Skills</a>
+    </p>
+    <p>
+        <img src="/blocks/succeed/images/maths-skills-icon.png" alt="" width="19" height="25" role="presentation">
+        <a href="https://learn.solent.ac.uk/maths-statistics">Maths and Numeracy</a>
+    </p>
+    <p>
+        <img src="/blocks/succeed/images/professional-practice-icon.png" alt="" width="21" height="25" role="presentation">
+        <a href="https://learn.solent.ac.uk/professional-presence">Professional Presence</a>
+    </p>
+    <br />
+    <h4>Research Methods</h4>
+    <p>
+        <img src="/blocks/succeed/images/statistics-icon.png" alt="" width="25" height="25" role="presentation">
+        <a href="https://learn.solent.ac.uk/quantitative-research-methods">Quantitative and Statistics</a>
+    </p>
+    ';
 
         return $this->content;
     }
